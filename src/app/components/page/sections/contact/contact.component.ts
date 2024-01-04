0c0f56d78af7971cc043bbf7b5e7e53df78e64f9
Dario Gutierrez Novoa <dario-yes@hotmail.com>
2024-01-03 21:00:51 -0500

    endfront

--- src/app/components/page/sections/contact/contact.component.ts
+++ src/app/components/page/sections/contact/contact.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, inject, OnInit} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+export class TuModulo { }
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
 
+  //creamos un formgroup
+  public formSend:FormGroup = new FormGroup({});
+  
+  //inyectamos el formbuilder
+  public formbuilder = inject(FormBuilder);
+  
+  //inicializamos el ngOnInit
+  ngOnInit(): void {
+    //Inicializamos el formgroup
+    this.formSend = this.formbuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      name: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      message: ['', Validators.compose([Validators.required, Validators.minLength(10)])],
+    });
+    //Nos suscribimos a los cambios que ocurran en el formulario y mostramos los datos
+    //this.formSend.valueChanges.subscribe(console.log);
+
+  }
+  //recogemos los datos del formulario
+  get email() { return this.formSend.get('email'); }
+  get name() { return this.formSend.get('name'); }
+  get message() { return this.formSend.get('message'); }
+  
+  //enviar datos
+  sendData(){
+    //controlar que el formulario sea valido
+    
+    if(this.formSend.valid){
+      //enviar datos
+      console.log(this.formSend.value);
+      //resetear los datos del formulario
+      this.formSend.reset();
+    }
+
+  }
 }
--- src/app/components/page/sections/nav/nav.component.ts
+++ src/app/components/page/sections/nav/nav.component.ts
@@ -1,14 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [
+    // importamos el modulo para usar el ngclass
+    CommonModule,
+  ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
 export class NavComponent {
 
   public project:boolean = false;
+  public menu:boolean = false;
+
+  //creamos una funcion que se encargue de ocultar el menu por medio de las clases de css
+  public hideMenu(){
+    this.menu = !this.menu;
+  }
 
 }
